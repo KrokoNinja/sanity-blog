7fe001b89fe2caec94f66c06b1832cd549490660
KrokoNinja <lenni.pfundtner@gmail.com>
2024-07-23 13:40:55 +0200

    finish single post page

--- app/(client)/posts/[slug]/page.tsx
+++ app/(client)/posts/[slug]/page.tsx
@@ -1,59 +1,84 @@
 import Header from "@/app/components/Header"
 import { Post } from "@/app/utils/interface";
 import { client } from "@/sanity/lib/client";
+import { urlFor } from "@/sanity/lib/image";
 import { PortableText } from "next-sanity";
-import {VT323} from "next/font/google"
+import { VT323 } from "next/font/google"
+import Image from "next/image";
 import Link from "next/link";
 
-const dateFont = VT323({weight: "400", subsets: ["latin"]});
+const dateFont = VT323({ weight: "400", subsets: ["latin"] });
 
 interface Params {
-  params: {
-    slug: string;
-  }
+    params: {
+        slug: string;
+    }
 }
 
 async function getPosts(slug: string) {
-  const query = `
+    const query = `
     *[_type == "post" && slug.current == "${slug}"][0]{
-      title,
-      slug,
-      publishedAt,
-      excerpt,
-      _id,
-      body,
-      tags[]->{
-        _id,
+        title,
         slug,
-        name
-      }
+        publishedAt,
+        excerpt,
+        _id,
+        body,
+        tags[]->{
+            _id,
+            slug,
+            name
+        }
     }`;
 
     const post = await client.fetch(query);
     return post;
 }
 
-async function page({params}: Params) {
+async function page({ params }: Params) {
 
-  const post: Post = await getPosts(params.slug);
+    const post: Post = await getPosts(params.slug);
 
-  return (
-    <div>
-      <Header title={post.title} />
-      <div className="text-center">
-        <span className={`${dateFont.className} text-purple-500`}>{new Date(post.publishedAt).toDateString()}</span>
-        <div className="mt-5">
-          {post.tags.map((tag) => (
-            <Link key={tag._id} href={`/tag/${tag.slug.current}`}>
-              <span className="mr-2 p-1 rounded-sm text-sm lowercase dark:bg-gray-950 border dark:border-gray-900">#{tag.name}</span>
-            </Link>
-          ))}
+    return (
+        <div>
+            <Header title={post.title} />
+            <div className="text-center">
+                <span className={`${dateFont.className} text-purple-500`}>{new Date(post.publishedAt).toDateString()}</span>
+                <div className="mt-5">
+                    {post.tags.map((tag) => (
+                        <Link key={tag._id} href={`/tag/${tag.slug.current}`}>
+                            <span className="mr-2 p-1 rounded-sm text-sm lowercase dark:bg-gray-950 border dark:border-gray-900">#{tag.name}</span>
+                        </Link>
+                    ))}
+                </div>
+                <div className={richTextStyles}>
+                    <PortableText
+                        value={post.body}
+                        components={myPortableTextComponents}
+                    />
+                </div>
+            </div>
         </div>
+    )
+}
+export default page
 
-        <PortableText value={post.body} />
-      </div>
-    </div>
-  )
+const myPortableTextComponents = {
+    types: {
+        image: ({ value }: any) => <Image src={urlFor(value).url()} alt="Post" width={700} height={700} />
+    },
 }
 
-export default page
+const richTextStyles = `
+mt-14
+text-justify
+max-w-2xl
+m-auto
+prose-headings:my-5
+prose-heading:text-2xl
+prose-p:mb-5
+prose-p:leading-7
+prose-li:list-disc
+prose-li:leading-7
+prose-li:ml-4
+`;
